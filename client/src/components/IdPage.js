64b24ee4d315d0534611d8e6aa1640ffb4dec49d
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-11-15 12:30:51 -0500

    mapping through my idpage

--- client/src/components/IdPage.js
+++ client/src/components/IdPage.js
@@ -6,183 +6,151 @@ import {Redirect} from 'react-router-dom'
 import styled from 'styled-components';
 
 const Follow = styled.div`
-.cf:before,
-.cf:after {
-    content:"";
-    display:table;
-}
-.cf:after {
-    clear:both;
-}
-
-/*--------------------------------------------------------------
-1.2 - FORMATTING
---------------------------------------------------------------*/
 body {
-  background: #343434 url(http://cameronbaney.com/codepen/twitter-widget/bg.jpg);
+  font-family: Arial, Helvetica, sans-serif;
+  margin-bottom: 100px;
+}
+
+h1 {
+  display: block;
+  font-size: 50px;
+  margin: 25px auto 0;
+  width: 975px;
+}
+
+h1>small {
+  color: #aaaaaa;
+  font-size: .5em;
+}
+
+header {
+  box-shadow: 1px 1px 4px rgba(0,0,0,0.5);
+  margin:   25px auto 50px;
+  height:   300px;
+  position: relative;
+  width:    975px;
+}
+
+figure.profile-banner {
+  left:     0;
+  overflow: hidden;
+  position: absolute;
+  top:      0;
+  z-index:  1;
+}
+
+figure.profile-picture {
+  background-position: center center;
   background-size: cover;
-  color: #4c4c4c;
-  font:300 1.1em/1.7em 'HelveticaNeue-Light', Helvetica, Arial, sans-serif;
+  border: 5px #efefef solid;
+  border-radius: 50%;
+  bottom: -50px;
+  box-shadow: inset 1px 1px 3px rgba(0,0,0,0.2), 1px 1px 4px rgba(0,0,0,0.3);
+  height: 148px;
+  left: 150px;
+  position: absolute;
+  width: 148px;
+  z-index: 3;
 }
 
-body > p {
-  bottom: 10px;
-    color: #fff;
-    font-size: 12px;
-    left: 10px;
-    position: absolute;
-}
-body > p a {
-    color: #fff
+div.profile-stats {
+  bottom: 0;
+  border-top: 1px solid rgba(0,0,0,0.5);
+  left: 0;
+  padding: 15px 15px 15px 350px;
+  position: absolute;
+  right: 0;
+  z-index: 2;
+  
+  /* Generated Gradient */
+  background: -moz-linear-gradient(top,  rgba(255,255,255,0.5) 0%, rgba(0,0,0,0.51) 3%, rgba(0,0,0,0.75) 61%, rgba(0,0,0,0.5) 100%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(255,255,255,0.5)), color-stop(3%,rgba(0,0,0,0.51)), color-stop(61%,rgba(0,0,0,0.75)), color-stop(100%,rgba(0,0,0,0.5)));
+  background: -webkit-linear-gradient(top,  rgba(255,255,255,0.5) 0%,rgba(0,0,0,0.51) 3%,rgba(0,0,0,0.75) 61%,rgba(0,0,0,0.5) 100%);
+ background: -o-linear-gradient(top,  rgba(255,255,255,0.5) 0%,rgba(0,0,0,0.51) 3%,rgba(0,0,0,0.75) 61%,rgba(0,0,0,0.5) 100%);
+  background: -ms-linear-gradient(top,  rgba(255,255,255,0.5) 0%,rgba(0,0,0,0.51) 3%,rgba(0,0,0,0.75) 61%,rgba(0,0,0,0.5) 100%);
+  background: linear-gradient(to bottom,  rgba(255,255,255,0.5) 0%,rgba(0,0,0,0.51) 3%,rgba(0,0,0,0.75) 61%,rgba(0,0,0,0.5) 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#80000000',GradientType=0 );
+
 }
 
-/*--------------------------------------------------------------
-2.0 - TWITTER WIDGET
---------------------------------------------------------------*/
-.twitter-widget {
-    color: #4c4c4c;
-    height: 249px;
-    left:50%;
-    margin: -125px 0 0 -223px;
-    position: absolute;
-    top:50%;
-    width:446px;
-    box-shadow: 1px 1px 16px rgba(0,0,0,.58);
-    border-radius:2px;
+div.profile-stats ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
 }
 
-/*--------------------------------------------------------------
-2.1 - TWITTER WIDGET HEADER
---------------------------------------------------------------*/
-.header {
-    background: url(http://cameronbaney.com/codepen/twitter-widget/bg-header.jpg) no-repeat;
-    color: #fdfdfd;
-    font: 12px/17px 'HelveticaNeue-UltraLight', Helvetica, Arial, sans-serif;
-    padding: 18px 28px 20px;
-    border-radius:2px 2px 0 0;
-}
-.avatar {
-    border: 4px solid #29c6f5;
-    border-top-width:3px;
-    float: left;
-    margin-right: 16px;
-    position: relative;
-    box-shadow: 0 1px 1px rgba(0,0,0,.6), 0 -1px 0 #9ce5fa;
-    border-radius: 2px
-}
-    .avatar::before {
-        content: "";
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        box-shadow: 0 1px 3px rgba(0,0,0,.4) inset;
-        -moz-box-shadow: 0 1px 3px rgba(0,0,0,.4) inset;
-        -webkit-box-shadow: 0 1px 3px rgba(0,0,0,.4) inset;
-    }
-    .avatar img {
-        display: block;
-        box-shadow: 0 0 2px rgba(0,0,0,.5) inset
-    }
-.header h2, .header p {
-    float: left;
-    width:288px;
-}
-    .header h2 {
-        font-size:26px;
-        line-height: 32px;
-        margin: 10px 0 8px
-    }
-
-/*--------------------------------------------------------------
-2.2 - TWITTER WIDGET STATS
---------------------------------------------------------------*/
-.stats {
-    background: rgb(243,243,243);
-    background: -moz-linear-gradient(top, rgba(243,243,243,1) 0%, rgba(236,236,237,1) 100%);
-    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(243,243,243,1)), color-stop(100%,rgba(236,236,237,1)));
-    background: -webkit-linear-gradient(top, rgba(243,243,243,1) 0%,rgba(236,236,237,1) 100%);
-    background: -o-linear-gradient(top, rgba(243,243,243,1) 0%,rgba(236,236,237,1) 100%);
-    background: -ms-linear-gradient(top, rgba(243,243,243,1) 0%,rgba(236,236,237,1) 100%);
-    background: linear-gradient(to bottom, rgba(243,243,243,1) 0%,rgba(236,236,237,1) 100%);
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f3f3f3', endColorstr='#ececed',GradientType=0 );
-    border-top:1px solid #fff;
-    border-bottom: 1px solid #d4d4d4;
-}
-.stat {
-    color: #4c4c4c;
-    float:left;
-    font-size: 14px;
-    line-height: 17px;
-    padding: 15px 0 14px;
-    text-align: center;
-    text-decoration: none;
-    text-shadow: 0 1px 0 #fff;
-    width:148px;
-}
-    .stat:first-child{
-        margin-left: 1px
-    }
-    .stat:hover {
-        color: #747474
-    }
-.stat strong {
-    display: block;
-    font-size: 25px;
-    line-height: 25px
+div.profile-stats ul li {
+  color: #efefef;
+  display: block;
+  float: left;
+  font-size: 24px;
+  font-weight: bold;
+  margin-right: 50px;
+  text-shadow: 1px 1px 2px rgba(0,0,0,0.7)
 }
 
-/*--------------------------------------------------------------
-2.3 - TWITTER WIDGET MENU
---------------------------------------------------------------*/
-.menu {
-    background: rgb(236,236,236);
-    background: -moz-linear-gradient(top, rgba(236,236,236,1) 0%, rgba(233,233,233,1) 86%, rgba(232,232,232,1) 94%, rgba(228,228,228,1) 100%);
-    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(236,236,236,1)), color-stop(86%,rgba(233,233,233,1)), color-stop(94%,rgba(232,232,232,1)), color-stop(100%,rgba(228,228,228,1)));
-    background: -webkit-linear-gradient(top, rgba(236,236,236,1) 0%,rgba(233,233,233,1) 86%,rgba(232,232,232,1) 94%,rgba(228,228,228,1) 100%);
-    background: -o-linear-gradient(top, rgba(236,236,236,1) 0%,rgba(233,233,233,1) 86%,rgba(232,232,232,1) 94%,rgba(228,228,228,1) 100%);
-    background: -ms-linear-gradient(top, rgba(236,236,236,1) 0%,rgba(233,233,233,1) 86%,rgba(232,232,232,1) 94%,rgba(228,228,228,1) 100%);
-    background: linear-gradient(to bottom, rgba(236,236,236,1) 0%,rgba(233,233,233,1) 86%,rgba(232,232,232,1) 94%,rgba(228,228,228,1) 100%);
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ececec', endColorstr='#e4e4e4',GradientType=0 );
-    border-top:1px solid #fff;
-    border-radius:0 0 2px 2px;
-    padding: 11px 0;
-    text-align: center
+div.profile-stats li span {
+  display: block;
+  font-size: 16px;
+  font-weight: normal;
 }
-.menu li {
-    display:inline-block;
-    margin: 0 34px
+
+div.profile-stats a.follow {
+  display: block;
+  float: right;color: #ffffff;
+  margin-top: 5px;
+  text-decoration: none;
+  
+  /* This is a copy and paste from Bootstrap */
+  background-color: #49afcd;
+  text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);
+  background-color: #49afcd;
+  background-image: -moz-linear-gradient(top, #5bc0de, #2f96b4);
+  background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#5bc0de), to(#2f96b4));
+  background-image: -webkit-linear-gradient(top, #5bc0de, #2f96b4);
+  background-image: -o-linear-gradient(top, #5bc0de, #2f96b4);
+  background-image: linear-gradient(to bottom, #5bc0de, #2f96b4);
+  background-repeat: repeat-x;
+  border-color: #2f96b4 #2f96b4 #1f6377;
+  border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ff5bc0de', endColorstr='#ff2f96b4', GradientType=0);
+  filter: progid:DXImageTransform.Microsoft.gradient(enabled=false);
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
 }
-.menu a {
-    background: url(http://cameronbaney.com/codepen/twitter-widget/icons.png) no-repeat;
-    display:block;
-    overflow: hidden;
-    text-indent: -9999px
+
+div.profile-stats a.follow.followed {
+  
+  /* Once again copied from Boostrap */
+  color: #ffffff;
+  text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);
+  background-color: #5bb75b;
+  background-image: -moz-linear-gradient(top, #62c462, #51a351);
+  background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#62c462), to(#51a351));
+  background-image: -webkit-linear-gradient(top, #62c462, #51a351);
+  background-image: -o-linear-gradient(top, #62c462, #51a351);
+  background-image: linear-gradient(to bottom, #62c462, #51a351);
+  background-repeat: repeat-x;
+  border-color: #51a351 #51a351 #387038;
+  border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ff62c462', endColorstr='#ff51a351', GradientType=0);
+  filter: progid:DXImageTransform.Microsoft.gradient(enabled=false);
 }
-    .menu .ico-compose {
-        background-position:-1px -2px;
-        width: 25px;
-        height: 26px;
-    }
-    .menu .ico-mentions {
-        background-position: -31px -1px;
-        width: 27px;
-        height: 28px;
-    }
-    .menu .ico-profile {
-        background-position: -65px -1px;
-        width: 24px;
-        height: 27px;
-    }
-    .menu .ico-settings {
-        background-position: -97px -2px;
-        width: 26px;
-        height: 27px;
-    }
-    .menu a:hover {
-        opacity:.75
-    }
+
+header>h1 {
+  bottom: -50px;
+  color: #354B63;
+  font-size: 40px;
+  left: 350px;
+  position: absolute;
+  z-index: 5;
+}
+
 `
 
 
@@ -252,150 +220,33 @@ class IdPage extends Component {
         return (
            <div>
                <Follow>
-               <div class="wrapper">
-  <header>
-    <a href="#" class="btn btn_left">
-      <span></span>
-      <span></span>
-      <span></span>
-    </a>
-    <div class="title">
-      <h1>Twitter</h1>
-    </div>
-    <a href="#" class="btn btn_right">
-      <i class="fa fa-pencil-square-o"></i>
-    </a>
-  </header>
-  <div class="banner">
-    <div class="avatar-container">
-      <a href="">
-        <img class="avatar" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/31787/profile/profile-512_3.jpg" alt="" />
-        <i class="fa fa-pencil"></i>
-      </a>
-    </div>
-  </div>
 
-  <div class="profile-info">
+               <h1>Be Happy <small>A social network for programmers.</small></h1>
+               {this.state.goals.map((goal) => {
+                    return (
+<header>
+<Link key={goal.id} to={`/${goal.id}`} />
+  <figure class="profile-banner">
+    <img src="https://unsplash.it/975/300" alt="Profile banner" />
+  </figure>
+  <figure class="profile-picture">
+   
+    
+  </figure>
+  <div class="profile-stats">
     <ul>
-      <li>
-        Tweets
-        <span>1,252</span>
-      </li>
-      <li>
-        Following
-        <span>371</span>
-      </li>
-      <li>
-        Followers
-        <span>1,252</span>
-      </li>    
+      <li>13    <span>Projects</span></li>
+      <li>1,354 <span>Commits</span></li>
+      <li>32    <span>Following</span></li>
+      <li>324   <span>Followers</span></li>
     </ul>
+    <a href="javascript:void(0);" class="follow">
+      Follow Nick
+    </a>
   </div>
-  <div class="profile-tweets">
-    <ul>
-      <li class="title">
-        Tweets
-      </li>
-      <li>
-        <img class="avatar" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/31787/profile/profile-512_3.jpg" alt="" />
-        <div class="tweet-content">        
-          <h5 class="tweet-user">
-            Hugo Darby-Brown
-            <span class="tweet-handle">
-              @DarbyBrown
-            </span>
-            <span class="tweet-date">
-              1h
-            </span>
-          </h5>
-          <p>
-           
-          </p>
-        </div>
-        
-      </li>
-      <li>
-        <img class="avatar" src="https://pbs.twimg.com/profile_images/3321327798/83ed9a740af0aa27fdcb3a35b1855085_bigger.jpeg" alt="" />
-        <div class="tweet-content">        
-          <h5 class="tweet-user">
-            Dillon de Voor   
-            <span class="tweet-handle">
-              ‏@CrocoDillon
-            </span>
-            <span class="tweet-date">
-              12h
-            </span>
-          </h5>
-          <p>
-           
-
-      
-          </p>
-        </div>
-      </li>      
-      <li>
-        <img class="avatar" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/31787/profile/profile-512_3.jpg" alt="" />
-        <div class="tweet-content">        
-          <h5 class="tweet-user">
-            Hugo Darby-Brown
-            <span class="tweet-handle">
-              @DarbyBrown
-            </span>
-            <span class="tweet-date">
-              9 Feb
-            </span>
-          </h5>
-          <p>
-            Selling an iPhone with flappy bird on it £10,000, and takers?
-          </p>
-        </div>
-      </li>
-      <li>
-        <img class="avatar" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/31787/profile/profile-512_3.jpg" alt="" />
-        <div class="tweet-content">        
-          <h5 class="tweet-user">
-            Hugo Darby-Brown
-            <span class="tweet-handle">
-              @DarbyBrown
-            </span>
-            <span class="tweet-date">
-              9 Feb
-            </span>
-          </h5>
-          <p>
-            <a>@thebabydino</a> <a>@CodePen</a> Thanks Ana :)
-          </p>
-        </div>
-      </li>
-
-      <li>
-        <img class="avatar" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/31787/profile/profile-512_3.jpg" alt="" />
-        <div class="tweet-content">        
-          <h5 class="tweet-user">
-            Hugo Darby-Brown
-            <span class="tweet-handle">
-              @DarbyBrown
-            </span>
-            <span class="tweet-date">
-              9 Feb
-            </span>
-          </h5>
-          <p>
-            Sass Rainbow Box-shadow Animation  - CodePen <a>https://codepen.io/hugo/pen/mqhHw</a>  via <a>@codepen</a>
-          </p>
-        </div>
-      </li>
-    </ul>
-  </div>
-
-<div class="compose-tweet">
-  <h3>Compose Tweet</h3>
-  <form action="#">
-    <textarea name="" id="" cols="30" rows="10"></textarea>
-    <input type="submit" value="tweet" />
-  </form>
-</div>
-</div>
+  <h1> Hey <small>nrdobie</small></h1>
+</header>
+               )})}     
     </Follow>
            </div>
         );
--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -144,25 +144,7 @@ class UserPage extends Component {
                         </figcaption>
 
                     </figure>
-                    {/* <figure className="snip1336 hover"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample74.jpg" alt="sample74" />
-                        <figcaption>
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample2.jpg" alt="profile-sample2" className="profile" />
-                            <h2>Wisteria Widget<span>Photographer</span></h2>
-                            <p>Calvin: I'm a genius, but I'm a misunderstood genius. Hobbes: What's misunderstood about you? Calvin: Nobody thinks I'm a genius.</p>
-                            <a href="#" className="follow">Follow</a>
-                            <a href="#" className="info">More Info</a>
-                        </figcaption>
-                    </figure>
-                    <figure className="snip1336"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample69.jpg" alt="sample69" />
-                        <figcaption>
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample5.jpg" alt="profile-sample5" className="profile" />
-                            <h2>Desmond Eagle<span>Accountant</span></h2>
-                            <p>If you want to stay dad you've got to polish your image. I think the image we need to create for you is "repentant but learning".</p>
-                            <a href="#" className="follow">Follow</a>
-                            <a href="#" className="info">More Info</a>
-                        </figcaption>
-
-                    </figure> */}
+                    {/*  */}
                     </div>)
                 })}
                 
